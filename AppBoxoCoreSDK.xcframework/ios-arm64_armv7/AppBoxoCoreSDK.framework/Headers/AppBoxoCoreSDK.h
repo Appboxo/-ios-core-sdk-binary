d08d475f0d53d25e0b2dd866e065033a72cc487a
Azamat Kushmanov <azamat.kushmanov@gmail.com>
2021-10-08 06:02:20 +0000

    updates AppboxoCoreSDK version to 1.4.9

---
+++ AppBoxoCoreSDK.xcframework/ios-arm64_armv7/AppBoxoCoreSDK.framework/Headers/AppBoxoCoreSDK.h
@@ -0,0 +1,18 @@
+//
+//  AppBoxoCoreSDK.h
+//  AppBoxoCoreSDK
+//
+//  Created by Azamat Kushmanov on 20/5/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppBoxoCoreSDK.
+FOUNDATION_EXPORT double AppBoxoCoreSDKVersionNumber;
+
+//! Project version string for AppBoxoCoreSDK.
+FOUNDATION_EXPORT const unsigned char AppBoxoCoreSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppBoxoCoreSDK/PublicHeader.h>
+
+
